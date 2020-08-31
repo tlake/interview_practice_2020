b8c77b4567d749f95511cb379dc55fca6bfa2c07
Tanner Lake <tanner.lake@gmail.com>
2020-08-30 20:39:58 -0700

    add skeleton for bst

--- pkg/datastructures/bst.go
+++ pkg/datastructures/bst.go
@@ -1,1 +1,37 @@
 package datastructures
+
+// BSTNode implements an individual element within a BST.
+type BSTNode struct {
+	data int
+}
+
+// BST implements a binary search tree.
+type BST struct{}
+
+// NewBSTNode returns an new BST node initialized with the given data.
+func NewBSTNode(data int) *BSTNode {
+	return &BSTNode{data}
+}
+
+// NewBST returns a new binary search tree.
+func NewBST() *BST {
+	return &BST{}
+}
+
+// Insert creates a BST node with the given data and inserts the node into the tree.
+func (b *BST) Insert(data int) {
+	node := NewBSTNode(data)
+	b.insert(node)
+}
+
+// Search walks the tree looking for the given data. If found, Search returns the node;
+// if not, Search returns nil.
+func (b *BST) Search(data int) *BSTNode {
+	return nil
+}
+
+// Delete removes the given node from the tree.
+func (b *BST) Delete(node *BSTNode) {}
+
+// insert is called by Insert, and does the work of adding the given node to the tree.
+func (b *BST) insert(node *BSTNode) {}
